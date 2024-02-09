3ca80bad2b20ca8a09b355ab7d1469c6a01f8b90
lis-dima <lisiev.dmytro@gmail.com>
2024-02-09 03:15:40 +0000

    Added element PlayAudio

---
+++ wwwroot/js/AudioControl.js
@@ -0,0 +1,31 @@
+﻿function playAudio(audioId) {
+    var audio = document.getElementById(audioId);
+    if (audio.paused) {
+        stopAllPlayingSounds();
+        var img = document.querySelector(`[attr-audio-id="${audioId}"]`);
+        img.src = "/img/icons/pause.png";
+        if (audio) {
+            audio.play();
+            audio.addEventListener("ended", function () {
+                img.src = "/img/icons/play.png";
+            });
+        }
+    } else {
+        PauseAudio(audioId);
+    }
+}
+
+function stopAllPlayingSounds() {
+    document.querySelectorAll('audio').forEach(function (audio) {
+        PauseAudio(audio.id);
+    });
+}
+
+function PauseAudio(audioId) {
+    var audio = document.getElementById(audioId);
+    var img = document.querySelector(`[attr-audio-id="${audio.id}"]`);
+    img.src = "/img/icons/play.png";
+    audio.pause();
+    audio.currentTime = 0;
+}
+
